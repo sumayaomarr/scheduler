ab35e2c8da0166ad7b52b9e4542be2d53f1e5969
sumaya <sumayaomarr@gmail.com>
2022-02-08 18:35:28 -0500

    updated bug to showcase show instead of confirmation page when error

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -43,10 +43,10 @@ describe("Form", () => {
     expect(onSave).not.toHaveBeenCalled();
   });
 
-  it("can successfully save after trying to submit an empty student name", () => {
+  it("can successfully save after trying to submit an empty student name but a given interviewer", () => {
     const onSave = jest.fn();
     const { getByText, getByPlaceholderText, queryByText } = render(
-      <Form interviewers={interviewers} onSave={onSave} />
+      <Form interviewers={interviewers} onSave={onSave} interviewer={2} />
     );
 
     fireEvent.click(getByText("Save"));
@@ -63,7 +63,7 @@ describe("Form", () => {
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
 
     expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 2);
   });
 
   it("calls onCancel and resets the input field", () => {
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,18 +4,17 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
   const transition = (newMode, replace = false) => {
+     setMode(newMode)
     if (replace) {
-      setMode((prev) => newMode)
-      let replaceHistory = [...history];
-    
-      replaceHistory.splice(replaceHistory.length, 1, newMode);
-  
-      setHistory (replaceHistory);
+      // let replaceHistory = [...history];
+      // replaceHistory.splice(replaceHistory.length, 1, newMode);
+      // setHistory (replaceHistory);
+      setHistory((prev)=> [...prev.slice(0, prev.length - 1), newMode])
     } else {
-      setMode((prev) => newMode);
-      let newHistory = [...history];
-      newHistory.push(newMode);
-      setHistory((prev) => newHistory);
+      // let newHistory = [...history];
+      // newHistory.push(newMode);
+      // setHistory((prev) => newHistory);
+      setHistory((prev)=> [...prev, newMode])
     }
   };
 
@@ -23,23 +22,10 @@ export default function useVisualMode(initial) {
   const back = () => {
     if (history.length > 1){
         setMode(history[history.length - 2])
-        setHistory((prev)=>[...prev.slice(0, -1)])
+        setHistory((prev)=>[...prev.slice(0, prev.length-1)])
       }
-    //  let newHistory = [...history];
-    // newHistory.pop(mode);
-    // setHistory((prev) => newHistory);
-    // if (history.length > 1) {
-    //   setMode((prev) => newHistory[(newHistory.length - 1)]);
-    // }
   };
 
 
   return { mode, transition, back };
 };
-
-// [1, 2 ,3, 4]
-// length = 4 
-// current = 4 index 3
-// need = 3 index 2
-// history[3] history[history.length-1]
-// history[2]history[history.length-2]
